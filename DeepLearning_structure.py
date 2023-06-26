f4ae4919f8c3e029a5356c88773ae2e7b2fe8355
zhang-qilong <121208526+zhang-qilong@users.noreply.github.com>
2023-06-26 10:56:40 +0800

    Update DeepLearning_structure.py

--- DeepLearning_structure.py
+++ DeepLearning_structure.py
@@ -20,6 +20,7 @@
 # import torch.nn.functional as F
 # from PIL import Image
 # import matplotlib.pyplot as plt
+#
 # from torchvision import transforms
 
 """---------------------Dataset--------------------"""
@@ -44,7 +45,8 @@
 #     """返回数据地址列表长度"""
 #     def __len__(self):
 #         return len(self.path_list)
-
+#
+#
 # # Ants dataset
 # ants_dataset = MyData(root_dir=r'D:\PyTorch_Python\Dataset_packing\hymenoptera_data\train',
 #                       label_dir=r'ants')
@@ -283,7 +285,14 @@
 # print(model, '\nn', Input)
 
 """---------------------简单CNN网络搭建--------------------"""
-# datasets = torchvision.datasets.CIFAR10("D:\\PyTorch_Python\\BCI_DataSets", train=True,
+# import torchvision
+# import torch.nn as nn
+# import torch
+# from torch.utils.tensorboard import SummaryWriter
+# from torchvision.transforms import transforms
+#
+#
+# datasets = torchvision.datasets.CIFAR10("D:\\PyTorch_Python\\Datas", train=True,
 #                                         transform=transforms.ToTensor(), download=True)
 #
 #
@@ -301,14 +310,14 @@
 #         # self.linear2 = nn.Linear(in_features=64, out_features=10)
 #         # self.flatten = Flatten()
 #
-#         self.model = Sequential(
+#         self.model = nn.Sequential(
 #             nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5, padding=2),
 #             nn.MaxPool2d(kernel_size=2),
 #             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=5, padding=2),
 #             nn.MaxPool2d(kernel_size=2),
 #             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, padding=2),
 #             nn.MaxPool2d(kernel_size=2),
-#             Flatten(),
+#             nn.Flatten(),
 #             nn.Linear(in_features=1024, out_features=64),
 #             nn.Linear(in_features=64, out_features=10)
 #         )
@@ -571,6 +580,9 @@ import torch.nn as nn
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+writer = SummaryWriter('ResNet')
 
 transform = transforms.Compose([transforms.Resize(40)
                                    , transforms.RandomHorizontalFlip()  # 随机水平翻转图片
@@ -578,31 +590,33 @@ transform = transforms.Compose([transforms.Resize(40)
                                    , transforms.ToTensor()
                                 ])
 
-train_datasets = dsets.CIFAR10(root=r'D:\PyTorch_Python\Dataset_packing', train=True
+train_datasets = dsets.CIFAR10(root=r'D:\PyTorch_Python\Datas', train=True
                                , transform=transform, download=True)
-tset_datasets = dsets.CIFAR10(root=r'D:\PyTorch_Python\Dataset_packing', train=False
+tset_datasets = dsets.CIFAR10(root=r'D:\PyTorch_Python\Datas', train=False
                               , transform=transform, download=True)
 
 train_loader = DataLoader(train_datasets, batch_size=128, shuffle=False)
 test_loader = DataLoader(tset_datasets, batch_size=128, shuffle=False)
 
 
+# 一个卷积核为3*3的卷积层
 def conv3x3(in_channels, out_channels, stride=1):
     return nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
 
 
 # 残差块
-class ResidualBlack(nn.Module):
+class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1, downsample=None):
         super().__init__()
         self.conv1 = conv3x3(in_channels, out_channels, stride)
-        self.bn1 = nn.BatchNorm1d(out_channels)
+        self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(in_channels, out_channels, stride)
+        self.conv2 = conv3x3(out_channels, out_channels)
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.downsample = downsample
 
     def forward(self, x):
+        residual = x
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
@@ -610,19 +624,59 @@ class ResidualBlack(nn.Module):
         out = self.bn2(out)
         # 判断残差块是否需要发挥作用
         if self.downsample:
-            
-            x = self.downsample(x)
-        out += x  # 比较有无残差快内卷积部分效果哪个好
+            residual = self.downsample(x)
+        out += residual  # 比较有无残差快内卷积部分效果哪个好
         out = self.relu(out)
         return out
 
 
 # 构建残差网络
-class Resnet(nn.Module):
-    def __init__(self):
+class ResNet(nn.Module):
+    def __init__(self, block, layers, num_classes=10):
         super().__init__()
+        self.in_channels = 16
+        self.conv = conv3x3(3, 16)
+        self.bn = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self.make_layer(block, 16, layers[0])
+        self.layer2 = self.make_layer(block, 32, layers[0], 2)
+        self.layer3 = self.make_layer(block, 64, layers[1], 2)
+        self.avg_pool = nn.AvgPool2d(8)
+        self.fc = nn.Linear(64, num_classes)
+
+    # 确定每个曾
+    def make_layer(self, block, out_channels, blocks, stride=1):
+        downsample = None
+        # 输入输出通道数不一致表示经过了卷积块
+
+        if (stride != 1) or (self.in_channels != out_channels):
+            downsample = nn.Sequential(conv3x3(self.in_channels, out_channels, stride=stride)
+                                       , nn.BatchNorm2d(out_channels))
+        layers = []
+        # 输入输出通道一致表示经过残差快
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels  # 上一块输出通道 = 下一块输入通道
+        for i in range(1, blocks):
+            layers.append(block(out_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.bn(out)
+        out = self.relu(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.avg_pool(out)
+        out = out.view(out.size(0), -1)  # 转化为一列
+        out = self.fc(out)
+        return out
 
 
+resnet = ResNet(ResidualBlock, [3, 3, 3])
+print(resnet)
+writer.add_graph(model=resnet, input_to_model=torch.ones(64, 3, 32, 32))
+writer.close()
 """---------------------循环神经网络、长短时记忆--------------------"""
 # """RNN MNIST手写字母识别"""
 # # Import packages
